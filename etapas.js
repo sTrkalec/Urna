cfb4784ebf09f41682ea4af7b865cecc7ab9aa11
sTrkalec <vytorgabriel123v@gmail.com>
2021-08-24 15:47:44 -0300

    Eletric ballot box desing created. Creating logic and animations JS

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38123,
+                nome: "Fulano Vereador1",
+                partido: "PT",
+                fotos: [
+                    {url: "v2.jpg", legenda: "Vereador"}
+                ]
+            },
+            {
+                numero: 38423,
+                nome: "Fulano Vereador2",
+                partido: "PSDB",
+                fotos: [
+                    {url: "v1.jpg", legenda: "Vereador"}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: "PRESIDENTE",
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 13,
+                nome: "LULA",
+                partido: "PT",
+                vice: "cic",
+                fotos: [
+                    {url: "l1.jpg", legenda: "PRESIDENTE"},
+                    {url: "v1.jpg", legenda: "VICE-PRESIDENTE", small:true}
+                ]
+            },
+            {
+                numero: 17,
+                nome: "BOLSONARO",
+                partido: "SLA",
+                vice: "Mouro",
+                fotos: [
+                    {url: "b2.jpg", legenda: "Vereador"},
+                    {url: "v1.jpg", legenda: "VICE-PRESIDENTE", small:true}
+                ]
+            }
+        ]
+    }
+]
